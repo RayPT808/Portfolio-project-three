fe1d210fdbf3865c4e58efa3e7ddf80f6e5d10b4
RayPT808 <pitti.ray@gmail.com>
2024-06-12 06:11:54 +0000

    Create get_tracker_data function to request input from the user.

--- run.py
+++ run.py
@@ -12,8 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('bmi_calculator')
 
-tracker = SHEET.worksheet('tracker')
 
-data = tracker.get_all_values()
+def get_tracker_data():
+    """
+    Get the trackers's figure based on the input from the user
+    """
 
-print(data)
+    print("Please enter your bodyweight in kilograms")
+    print("Data should be six numbers, separated by commas")
+    print("Example: 55,62,77,69,91,83\n")
+
+    data_str = input("Enter your weight here: ")
+    print(f"The data provided is {data_str}")
+
+get_tracker_data()
